5e5dadf84bea26a6024be42c424358b47e1433b0
Дима <@DESKTOP-E2NI005>
2021-02-07 00:38:19 +0300

    Создание проекта.

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<!doctype html>
+<html>
+    <body>
+        <h1>Лок. сайт</h1>
+        <p>В разработке.</p>
+    </body>
+</html>
