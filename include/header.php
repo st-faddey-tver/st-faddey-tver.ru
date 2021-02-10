0f642afdceb0260d5bd40c246c2fda2142278af3
Дима <@DESKTOP-E2NI005>
2021-02-11 00:56:36 +0300

    Шапка.

--- include/header.php
+++ include/header.php
@@ -4,7 +4,7 @@
         <div class="ikona_top">
             <img src="<?=APPLICATION ?>/images/ikona.jpg" class="img-fluid" />
         </div>
-        <div style="position: absolute; left: 10%; bottom: 5%; width: 30%; padding: 12px; background-color: white; opacity: 0.6; border-radius: 15px; border: solid 2px #913d14; font-weight: bold;">
+        <div class="address_top">
             170007, г. Тверь, ул. Кропоткина, кладбище Неопалимой Купины 
         </div>
     </div>
