ae7fda554e22452935522edc828173a192175adf
Дима <@DESKTOP-E2NI005>
2021-02-13 22:27:59 +0300

    Футер.

--- include/footer.php
+++ include/footer.php
@@ -1,22 +1,43 @@
 <div class="container">
     <div class="footer">
         <div class="row">
-            <div class="col-3">
+            <div class="col-2">
                 <table>
                     <tr>
                         <td style="vertical-align: top; padding-right: 10px;"><i class="far fa-copyright"></i></td>
-                        <td>Храм свмч. Фаддея<br /> архиепископа Тверского</td>
+                        <td>Храм свмч. Фаддея архиепископа Тверского</td>
                     </tr>
                 </table>
             </div>
             <div class="col-2">
                 <p><a href="<?=APPLICATION ?>/">На главную</a></p>
+                <p><a href="<?=APPLICATION ?>/pilgrimage/">Паломничество</a></p>
                 <p><a href="<?=APPLICATION ?>/contact/">Контакты</a></p>
                 <p><a href="<?=APPLICATION ?>/gallery/">Фотогалерея</a></p>
             </div>
             <div class="col-2">
                 <p>О храме</p>
                 <p><a href="<?=APPLICATION ?>/about/history/">История</a></p>
+                <p><a href="<?=APPLICATION ?>/about/saints/">Святые храма</a></p>
+                <p><a href="<?=APPLICATION ?>/about/schedule/">Расписание богослужений</a></p>
+                <p><a href="<?=APPLICATION ?>/about/clergy/">Духовенство</a></p>
+            </div>
+            <div class="col-2">
+                <p>Молодёжь храма</p>
+                <p><a href="<?=APPLICATION ?>/youth/volunteer/">Добровольческое движение</a></p>
+                <p><a href="<?=APPLICATION ?>/youth/club/">Молодёжный клуб &laquo;Встреча&raquo;</a></p>
+                <p><a href="<?=APPLICATION ?>/youth/family/">Семейный клуб</a></p>
+                <p><a href="<?=APPLICATION ?>/youth/cinema/">Синематографический клуб</a></p>
+            </div>
+            <div class="col-2">
+                <p>Воскресная школа</p>
+                <p><a href="<?=APPLICATION ?>/education/sunday/schedule/">Расписание занятий</a></p>
+                <p><a href="<?=APPLICATION ?>/education/sunday/documents/">Документы</a></p>
+                <p><a href="<?=APPLICATION ?>/education/sunday/teachers/">Преподаватели</a></p>
+                <p>Курсы подготовки для поступления в семинарию</p>
+                <p><a href="<?=APPLICATION ?>/education/preparation/schedule/">Расписание занятий</a></p>
+                <p><a href="<?=APPLICATION ?>/education/preparation/documents/">Документы</a></p>
+                <p><a href="<?=APPLICATION ?>/education/preparation/teachers/">Преподаватели</a></p>
             </div>
         </div>
     </div>
