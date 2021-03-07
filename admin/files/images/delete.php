e72d54950aa6deab1007a805bc2f393376965777
Дима <@DESKTOP-E2NI005>
2021-03-07 16:11:42 +0300

    Изображения.

---
+++ admin/files/images/delete.php
@@ -0,0 +1,78 @@
+<?php
+include '../../../include/topscripts.php';
+
+// Авторизация
+if(!IsInRole(array('files', 'admin'))) {
+    header('Location: '.APPLICATION.'/admin/login.php');
+}
+
+// Если не указан id, переходим к списку
+$id = filter_input(INPUT_GET, 'id');
+if(empty($id)) {
+    header('Location: '.APPLICATION.'/admin/files/images/');
+}
+
+// Обработка отправки формы
+if(null !== filter_input(INPUT_POST, 'image_section_delete_submit')) {
+    $id = filter_input(INPUT_POST, 'id');
+    $sql = "delete from image_section where id=$id";
+    $error_message = (new Executer($sql))->error;
+    
+    if(empty($error_message)) {
+        header('Location: '.APPLICATION.'/admin/files/images/');
+    }
+}
+
+// Получение объекта
+$name = '';
+$sql = "select name from image_section where id=$id";
+$fetcher = new Fetcher($sql);
+$error_message = $fetcher->error;
+
+if($row = $fetcher->Fetch()) {
+    $name = $row['name'];
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        include '../../include/head.php';
+        ?>
+    </head>
+    <body>
+        <?php
+        include '../../include/header.php';
+        ?>
+        <div class="container-fluid">
+            <?php
+            if(!empty($error_message)) {
+                echo "<div class='alert alert-danger'>$error_message</div>";
+            }
+            ?>
+            <ul class="breadcrumb">
+                <li><a href="<?=APPLICATION ?>/">На главную</a></li>
+                <li><a href="<?=APPLICATION ?>/admin/">Администратор</a></li>
+                <li><a href="<?=APPLICATION ?>/admin/files/images/">Изображения</a></li>
+                <li><a href="<?=APPLICATION ?>/admin/files/images/details.php<?= BuildQuery("id", $id) ?>"><?=$name ?></a></li>
+                <li>Удаление раздела</li>
+            </ul>
+            <div class="d-flex justify-content-between mb-2">
+                <div class="p-1">
+                    <h2 class="text-danger">Действительно удалить?</h2>
+                </div>
+                <div class="p-1">
+                    <a href="details.php<?= BuildQuery('id', $id) ?>" class="btn btn-outline-dark"><i class="fas fa-undo-alt"></i>&nbsp;Отмена</a>
+                </div>
+            </div>
+            <h1><?=$name ?></h1>
+            <form method="post">
+                <input type="hidden" id="id" name="id" value="<?= filter_input(INPUT_GET, 'id') ?>" />
+                <button type="submit" id="image_section_delete_submit" name="image_section_delete_submit" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i>&nbsp;Удалить</button>
+            </form>
+        </div>
+        <?php
+        include '../../include/footer.php';
+        ?>
+    </body>
+</html>
---
+++ admin/files/images/edit.php
@@ -0,0 +1,107 @@
+<?php
+include '../../../include/topscripts.php';
+
+// Авторизация
+if(!IsInRole(array('files', 'admin'))) {
+    header('Location: '.APPLICATION.'/admin/login.php');
+}
+
+// Если не указан id, переходим к списку
+$id = filter_input(INPUT_GET, 'id');
+if(empty($id)) {
+    header('Location: '.APPLICATION.'/admin/files/images/');
+}
+
+// Валидация формы
+define('ISINVALID', ' is-invalid');
+$form_valid = true;
+$error_message = '';
+
+$name_valid = '';
+
+// Обработка отправки формы
+if(null !== filter_input(INPUT_POST, 'image_section_edit_submit')) {
+    if(empty(filter_input(INPUT_POST, 'name'))) {
+        $name_valid = ISINVALID;
+        $form_valid = false;
+    }
+    
+    if($form_valid) {
+        $id = filter_input(INPUT_POST, 'id');
+        $name = addslashes(filter_input(INPUT_POST, 'name'));
+        $sql = "update image_section set name='$name' where id=$id";        echo $sql;
+        $error_message = (new Executer($sql))->error;
+        
+        if(empty($error_message)) {
+            header('Location: '.APPLICATION."/admin/files/images/details.php".BuildQuery('id', $id));
+        }
+    }
+}
+
+// Получение объекта
+$id = filter_input(INPUT_GET, 'id');
+if(empty($id)) {
+    $id = filter_input(INPUT_GET, 'id');
+}
+
+$sql = "select name from image_section where id=$id";
+$row = (new Fetcher($sql))->Fetch();
+
+$name = filter_input(INPUT_POST, 'name');
+if(empty($name)) {
+    $name = $row['name'];
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        include '../../include/head.php';
+        ?>
+    </head>
+    <body>
+        <?php
+        include '../../include/header.php';
+        ?>
+        <div class="container-fluid">
+            <?php
+            if(!empty($error_message)) {
+                echo "<div class='alert alert-danger'>$error_message</div>";
+            }
+            ?>
+            <ul class="breadcrumb">
+                <li><a href="<?=APPLICATION ?>/">На главную</a></li>
+                <li><a href="<?=APPLICATION ?>/admin/">Администратор</a></li>
+                <li><a href="<?=APPLICATION ?>/admin/files/images/">Изображения</a></li>
+                <li><a href="<?=APPLICATION ?>/admin/files/images/details.php<?= BuildQuery("id", $id) ?>"><?=$name ?></a></li>
+                <li>Редактирование раздела</li>
+            </ul>
+            <div class="d-flex justify-content-between mb-2">
+                <div class="p-1">
+                    <h1>Редактирование раздела</h1>
+                </div>
+                <div class="p-1">
+                    <a href="details.php<?= BuildQuery('id', $id) ?>" class="btn btn-outline-dark"><i class="fas fa-undo-alt"></i>&nbsp;Отмена</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <form method="post">
+                        <input type="hidden" id="id" name="id" value="<?=$id ?>" />
+                        <div class="form-group">
+                            <label for="name">Наименование</label>
+                            <input type="text" id="name" name="name" class="form-control<?=$name_valid ?>" value="<?= htmlentities($name) ?>" required="required" />
+                            <div class="invalid-feedback">Наименование обязательно</div>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" id="image_section_edit_submit" name="image_section_edit_submit" class="btn btn-outline-dark"><i class="fas fa-save"></i>&nbsp;Сохранить</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <?php
+        include '../../include/footer.php';
+        ?>
+    </body>
+</html>
