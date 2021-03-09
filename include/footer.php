eb7352302703f9a7c945baa82af9cdc32bad2bce
Дима <@DESKTOP-E2NI005>
2021-03-10 00:34:29 +0300

    footer

--- include/footer.php
+++ include/footer.php
@@ -22,11 +22,17 @@
             <div class="col-2">
                 <p>О храме</p>
                 <p><a href="<?=APPLICATION ?>/history/">История</a></p>
-                <p><a href="<?=APPLICATION ?>/saints/">Святые храма</a></p>
                 <p><a href="<?=APPLICATION ?>/schedule/">Расписание богослужений</a></p>
                 <p><a href="<?=APPLICATION ?>/clergy/">Духовенство</a></p>
                 <p><a href="<?=APPLICATION ?>/events/">Все события</a></p>
                 <p><a href="<?=APPLICATION ?>/news/">Все новости</a></p>
+            </div>
+            <div class="col-2">
+                <p>Святые храма</p>
+                <p><a href="<?=APPLICATION ?>/nikolay_maslov/">Николай Маслов</a></p>
+                <p><a href="<?=APPLICATION ?>/ilya_gromoglasov/">Илья Громогласов</a></p>
+                <p><a href="<?=APPLICATION ?>/ilya_benemansky/">Илья Бенеманский</a></p>
+                <p><a href="<?=APPLICATION ?>/faddey_uspensky/">Фаддей Успенский</a></p>
             </div>
             <div class="col-2">
                 <p>Молодёжь храма</p>
@@ -36,16 +42,6 @@
                 <p><a href="<?=APPLICATION ?>/cinema/">Синематографический клуб</a></p>
             </div>
             <div class="col-2">
-                <p>Воскресная школа</p>
-                <p><a href="<?=APPLICATION ?>/sunday/schedule/">Расписание занятий</a></p>
-                <p><a href="<?=APPLICATION ?>/sunday/documents/">Документы</a></p>
-                <p><a href="<?=APPLICATION ?>/sunday/teachers/">Преподаватели</a></p>
-            </div>
-            <div class="col-2">
-                <p>Курсы подготовки для поступления в семинарию</p>
-                <p><a href="<?=APPLICATION ?>/preparation/schedule/">Расписание занятий</a></p>
-                <p><a href="<?=APPLICATION ?>/preparation/documents/">Документы</a></p>
-                <p><a href="<?=APPLICATION ?>/preparation/teachers/">Преподаватели</a></p>
             </div>
         </div>
     </div>
