8f88475e4d0958e3d6a193c638c9cf030168526c
Дима <@DESKTOP-E2NI005>
2021-03-10 21:15:44 +0300

    events

--- pages/about/events/details.php
+++ pages/about/events/details.php
@@ -19,34 +19,6 @@ if(empty($error_message) && $row = $fetcher->Fetch()) {
     $title = $row['title'];
 }
 
-if(empty($error_message)) {
-    $sql = "select shortname, title from news where is_event = 1 and (date < '$date' or (date = '$date' and id < $id)) order by date desc, id desc limit 1";
-    $fetcher = new Fetcher($sql);
-    $error_message = $fetcher->error;
-}
-
-$previous_shortname = '';
-$previous_title = '';
-
-if(empty($error_message) && $row = $fetcher->Fetch()) {
-    $previous_shortname = $row['shortname'];
-    $previous_title = $row['title'];
-}
-
-if(empty($error_message)) {
-    $sql = "select shortname, title from news where is_event = 1 and (date > '$date' or (date = '$date' and id > $id)) order by date asc, id asc limit 1";
-    $fetcher = new Fetcher($sql);
-    $error_message = $fetcher->error;
-}
-
-$next_shortname = '';
-$next_title = '';
-
-if(empty($error_message) && $row = $fetcher->Fetch()) {
-    $next_shortname = $row['shortname'];
-    $next_title = $row['title'];
-}
-
 $news = new News($id);
 ?>
 <!DOCTYPE html>
@@ -67,24 +39,6 @@ $news = new News($id);
                     echo "<div class='alert alert-danger'>$error_message</div>";
                 }
                 ?>
-                <div class="row">
-                    <div class="col-4 text-left">
-                        <?php if(!empty($next_shortname)): ?>
-                        <div class="news_title"><a href="<?=APPLICATION ?>/events/<?=$next_shortname ?>" title="<?=$next_title ?>" data-toggle="tooltip" data-placement="right"><i class="fas fa-arrow-left"></i></a></div>
-                        <?php endif; ?>
-                    </div>
-                    <div class="col-4 text-center">
-                        <div class="news_title">
-                            <a href="<?=APPLICATION ?>/events/">Все события</a>
-                        </div>
-                    </div>
-                    <div class="col-4 text-right">
-                        <?php if(!empty($previous_shortname)): ?>
-                        <div class="news_title"><a href="<?=APPLICATION ?>/events/<?=$previous_shortname ?>" title="<?=$previous_title ?>" data-toggle="tooltip" data-placement="left"><i class="fas fa-arrow-right"></i></a></div>
-                        <?php endif; ?>
-                    </div>
-                </div>
-                <hr />
                 <p><i><?= DateTime::createFromFormat('Y-m-d', $date)->format('d.m.Y') ?></i></p>
                 <h1><?=$title ?></h1>
                 <?php
