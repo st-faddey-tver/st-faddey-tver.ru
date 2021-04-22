9ab05c9eabe8327e96e08547e69ca3877b075828
Дима <@DESKTOP-T0PTTL3>
2021-04-22 22:49:40 +0300

    События

--- admin/index.php
+++ admin/index.php
@@ -55,40 +55,6 @@ if(!HasRole()) {
                     <?php
                     endwhile;
                     ?>
-                    <hr/>
-                    <p>О храме</p>
-                    <ul>
-                        <li><a href="page.php?shortname=history">История</a></li>
-                        <li>
-                            Святые храма
-                            <ul>
-                                <li><a href="page.php?shortname=vera_truks">Вера Трукс</a></li>
-                                <li><a href="page.php?shortname=ilya_benemansky">Илья Бенеманский</a></li>
-                                <li><a href="page.php?shortname=ilya_gromoglasov">Илья Громогласов</a></li>
-                                <li><a href="page.php?shortname=nikolay_maslov">Николай Маслов</a></li>
-                                <li><a href="page.php?shortname=faddey_uspensky">Фаддей Успенский</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="page.php?shortname=schedule">Расписание богослужений</a></li>
-                        <li><a href="page.php?shortname=clergy">Духовенство</a></li>
-                        <li><a href="news/<?= BuildQuery('is_event', 1) ?>">Все события</a></li>
-                        <li><a href="news/<?= BuildQuery('is_event', 0) ?>">Все новости</a></li>
-                    </ul>
-                    <p>Молодёжь храма</p>
-                    <ul>
-                        <li><a href="page.php?shortname=volunteer">Добровольческое движение</a></li>
-                        <li><a href="page.php?shortname=club">Молодёжный клуб &laquo;Встреча&raquo;</a></li>
-                        <li><a href="page.php?shortname=family">Семейный клуб</a></li>
-                        <li><a href="page.php?shortname=cinema">Синематографический клуб</a></li>
-                    </ul>
-                    <p>Образовательный центр</p>
-                    <ul>
-                        <li><a href="page.php?shortname=sunday">Воскресная школа</a></li>
-                        <li><a href="page.php?shortname=preparation">Курсы подготовки к поступлению в семинарию</a></li>
-                    </ul>
-                    <p><a href="page.php?shortname=pilgrimage">Паломничество</a></p>
-                    <p><a href="page.php?shortname=contact">Контакты</a></p>
-                    <p><a href="page.php?shortname=donation">Пожертвовать</a></p>
                 </div>
                 <div class="col-6">
                     <p><a href="event/">Все события</a>
