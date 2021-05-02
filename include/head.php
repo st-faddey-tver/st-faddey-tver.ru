60bc67564d4bf0a2d5cd3548c507e059041e2d72
Дима <@DESKTOP-T0PTTL3>
2021-05-02 19:07:48 +0300

    фавикон

--- include/head.php
+++ include/head.php
@@ -5,7 +5,7 @@
 <link href="<?=APPLICATION ?>/fancybox-master/dist/jquery.fancybox.min.css" rel="stylesheet" />
 <link href="<?=APPLICATION ?>/css/main.css?version=8" rel="stylesheet" />
 
-<link rel="shortcut icon" type="image/x-icon" href="<?=APPLICATION ?>/favicon.ico" />
+<link rel="icon" type="image/x-icon" href="<?=$_SERVER['REQUEST_SCHEME'].'://'. $_SERVER['HTTP_HOST'].APPLICATION ?>/favicon.ico" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <?php
 const TITLE = "Храм священномученика Фаддея архиепископа Тверского";
