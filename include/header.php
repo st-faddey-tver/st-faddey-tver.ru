ccc6cc5215066e9c3d206612c4167e767b83a93e
Дима <@DESKTOP-T0PTTL3>
2022-03-16 22:46:46 +0300

    2022_03_16 Свщмч

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
 <div class="top" id="top" style="background-image: url(<?=APPLICATION ?>/images/shapka_zima.jpg); background-position-x: center; background-position-y: top; background-size: cover;">
     <div class="container" style="margin-bottom: 0;">
         <div class="header">
-            <a href="<?=APPLICATION ?>/"><img src="<?=APPLICATION ?>/images/header_prozrachny.png" class="img-fluid d-none d-md-block" /></a>
+            <a href="<?=APPLICATION ?>/"><img src="<?=APPLICATION ?>/images/header_prozrachny.png?date=20220316" class="img-fluid d-none d-md-block" /></a>
             <div class="d-none d-md-block" style="font-family: Times New Roman, Times, serif; position: absolute; top: 50%; left: 1%; color: #913d14; font-size: large; line-height: normal; font-style: italic; font-weight: bold; width: 25%; text-align: left;">
                 &#8222;Сними обувь твою с ног твоих, ибо место, на котором ты стоишь, есть земля святая.&#8220;
                 <div style="text-align: right; font-size: smaller;">Исх 3:5</div>
@@ -16,7 +16,7 @@
                     <a href="<?=APPLICATION ?>/"><img src="<?=APPLICATION ?>/images/petrov_image_400.png" class="img-fluid" /></a>
                 </div>
                 <div class="col-8 pt-2">
-                    <a href="<?=APPLICATION ?>/"><img src="<?=APPLICATION ?>/images/header_slav_transparent.png" class="img-fluid" /></a>
+                    <a href="<?=APPLICATION ?>/"><img src="<?=APPLICATION ?>/images/header_slav_transparent.png?date=20220316" class="img-fluid" /></a>
                 </div>
             </div>
             
