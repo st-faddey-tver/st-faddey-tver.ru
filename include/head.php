6588e40f00c0ece388fb5678abc2063921d97218
Дмитрий <@DESKTOP-TJ6IVLC>
2023-11-02 19:41:00 +0300

    02.11.2023 - favicon

--- include/head.php
+++ include/head.php
@@ -5,8 +5,9 @@
 <link href="<?=APPLICATION ?>/fancybox-master/dist/jquery.fancybox.min.css" rel="stylesheet" />
 <link href="<?=APPLICATION ?>/css/main.css?version=10" rel="stylesheet" />
 
-<link rel="icon" type="image/x-icon" href="<?=$_SERVER['REQUEST_SCHEME'].'://'. $_SERVER['HTTP_HOST'].APPLICATION ?>/favicon.ico" />
-<link rel="icon" type="image/svg+xml" href="<?=$_SERVER['REQUEST_SCHEME'].'://'. $_SERVER['HTTP_HOST'].APPLICATION ?>/favicon.svg" />
+<link rel="icon" type="image/png" sizes="16x16" href="<?=$_SERVER['REQUEST_SCHEME'].'://'. $_SERVER['HTTP_HOST'].APPLICATION ?>/favicon/favicon-16x16.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="<?=$_SERVER['REQUEST_SCHEME'].'://'. $_SERVER['HTTP_HOST'].APPLICATION ?>/favicon/favicon-32x32.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="<?=$_SERVER['REQUEST_SCHEME'].'://'. $_SERVER['HTTP_HOST'].APPLICATION ?>/favicon/apple-touch-icon.png">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <?php
 const TITLE = "Храм священномученика Фаддея архиепископа Тверского";
