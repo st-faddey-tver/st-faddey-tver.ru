ce722e91f5341a787629612de87a9a3529f20c6d
Дмитрий <@DESKTOP-TJ6IVLC>
2025-08-30 00:10:55 +0300

    26.08.2025 - H2

--- include/head.php
+++ include/head.php
@@ -3,7 +3,7 @@
 <link href="<?=APPLICATION ?>/css/bootstrap.min.css" rel="stylesheet">
 <link href="<?=APPLICATION ?>/fontawesome-free-5.15.2-web/css/all.min.css" rel="stylesheet" />
 <link href="<?=APPLICATION ?>/fancybox-master/dist/jquery.fancybox.min.css" rel="stylesheet" />
-<link href="<?=APPLICATION ?>/css/main.css?version=15" rel="stylesheet" />
+<link href="<?=APPLICATION ?>/css/main.css?version=16" rel="stylesheet" />
 
 <link rel="icon" type="image/png" sizes="16x16" href="<?=$_SERVER['REQUEST_SCHEME'].'://'. $_SERVER['HTTP_HOST'].APPLICATION ?>/favicon/favicon-16x16.png" />
 <link rel="icon" type="image/png" sizes="32x32" href="<?=$_SERVER['REQUEST_SCHEME'].'://'. $_SERVER['HTTP_HOST'].APPLICATION ?>/favicon/favicon-32x32.png" />
